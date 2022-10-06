7dcd9a1e17a440814dae07dd7cd42e7c7ab5943a
lauraapereez <115158284+lauraapereez@users.noreply.github.com>
2022-10-06 14:32:04 +0200

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+var mascota = "perro";
+var total_mascotas = 2;
+var encendido = true;
+
+console.log("los topos de datos son")
+console.log(typeof mascota);
+console.log(typeof total_mascotas);
+console.log(typeof encendido);
